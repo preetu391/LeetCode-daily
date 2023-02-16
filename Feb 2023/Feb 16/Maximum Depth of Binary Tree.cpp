24e98310262c95eaceb9a9cbb0aedd1057c587f2
preetu391 <preetu391391@gmail.com>
2023-02-16 17:40:35 +0530

    Feb 16

---
+++ Feb 2023/Feb 16/Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode *root)
+{
+    if (root == NULL)
+        return 0;
+    return max(maxDepth(root->left) + 1, maxDepth(root->right) + 1);
+}
+
+int main()
+{
+
+    return 0;
+}
