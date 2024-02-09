9331975f3cdfcae78264cb8a38347e528328ac5f
preetu391 <preetu391391@gmail.com>
2024-02-09 07:54:54 +0530

    Feb 9

---
+++ Feb 2024/Largest Divisible Subset.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> largestDivisibleSubset(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int> dp(n, 1), bt(n);
+        int max_len = 1, prev_ind = 0;
+        for (int i = 0; i < n; i++)
+        {
+            bt[i] = i;
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] % nums[j] == 0 && 1 + dp[j] > dp[i])
+                {
+                    dp[i] = 1 + dp[j];
+                    bt[i] = j;
+                }
+            }
+            if (dp[i] > max_len)
+            {
+                max_len = dp[i];
+                prev_ind = i;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(nums[prev_ind]);
+        while (bt[prev_ind] != prev_ind)
+        {
+            prev_ind = bt[prev_ind];
+            ans.push_back(nums[prev_ind]);
+        }
+        return ans;
+    }
+};
